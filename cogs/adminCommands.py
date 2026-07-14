c73a378a67b0cbf1b1df92ca78e27eb09b36be70
oarmdc <gallaouimohamedomar@gmail.com>
2026-07-14 21:24:29 +0200

    unload wrong comment

--- cogs/adminCommands.py
+++ cogs/adminCommands.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 from utils import make_embed, send_error_embed, send_success_embed
 
@@ -6,41 +7,37 @@ class adminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def ban(self, ctx, member: discord.Member, *, reason=None):
-        if ctx.author.id != ctx.guild.owner_id and member.id == ctx.guild.owner_id:
-            await send_error_embed(ctx, "You can't ban the owner, man..")
+    @app_commands.command(description="Ban a member from the server")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str = None):
+        if interaction.user.id != interaction.guild.owner_id and member.id == interaction.guild.owner_id:
+            await send_error_embed(interaction, "You can't ban the owner, man..")
             return
-        if ctx.author.id != ctx.guild.owner_id and member.guild_permissions.administrator:
-            await send_error_embed(ctx, "You can't ban another admin.")
+        if interaction.user.id != interaction.guild.owner_id and member.guild_permissions.administrator:
+            await send_error_embed(interaction, "You can't ban another admin.")
             return
         await member.ban(reason=reason)
-        await send_success_embed(ctx, f"{member.name} is now banned.")
+        await send_success_embed(interaction, f"{member.name} is now banned.")
     @ban.error
-    async def ban_error(self, ctx, error):
-        if isinstance(error, commands.MissingPermissions):
-            await send_error_embed(ctx, "You don't have permission to use this command.")
-        elif isinstance(error, commands.MemberNotFound):
-            await send_error_embed(ctx, "That user isn't in this server.")
+    async def ban_error(self, interaction, error):
+        if isinstance(error, app_commands.MissingPermissions):
+            await send_error_embed(interaction, "You don't have permission to use this command.")
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def kick(self, ctx, member: discord.Member, *, reason=None):
-        if ctx.author.id != ctx.guild.owner_id and member.id == ctx.guild.owner_id:
-            await send_error_embed(ctx, "You can't kick the owner, man..")
+    @app_commands.command(description="Kick a member from the server")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def kick(self, interaction: discord.Interaction, member: discord.Member, reason: str = None):
+        if interaction.user.id != interaction.guild.owner_id and member.id == interaction.guild.owner_id:
+            await send_error_embed(interaction, "You can't kick the owner, man..")
             return
-        if ctx.author.id != ctx.guild.owner_id and member.guild_permissions.administrator:
-            await send_error_embed(ctx, "You can't kick another admin.")
+        if interaction.user.id != interaction.guild.owner_id and member.guild_permissions.administrator:
+            await send_error_embed(interaction, "You can't kick another admin.")
             return
         await member.kick(reason=reason)
-        await send_success_embed(ctx, f"{member.name} is now kicked.")
+        await send_success_embed(interaction, f"{member.name} is now kicked.")
     @kick.error
-    async def kick_error(self, ctx, error):
-        if isinstance(error, commands.MissingPermissions):
-            await send_error_embed(ctx, "You don't have permission to use this command.")
-        elif isinstance(error, commands.MemberNotFound):
-            await send_error_embed(ctx, "That user isn't in this server.")
+    async def kick_error(self, interaction, error):
+        if isinstance(error, app_commands.MissingPermissions):
+            await send_error_embed(interaction, "You don't have permission to use this command.")
 
 async def setup(bot):
     await bot.add_cog(adminCommands(bot))
