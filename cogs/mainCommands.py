06ab90e54cd4911236537a115436d36404ab83e6
oarmdc <gallaouimohamedomar@gmail.com>
2026-07-16 00:21:09 +0200

    migrate pfp command to app_commands for improved interaction handling

--- cogs/mainCommands.py
+++ cogs/mainCommands.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 from utils import make_embed
 
@@ -6,11 +7,11 @@ class mainCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
-    async def pfp(self, ctx, thatuser: discord.Member = None):
-        target = thatuser or ctx.author
-        embed = make_embed(ctx, f"{target.name}'s avatar", image=target.display_avatar.url, color=discord.Color.blue())
-        await ctx.send(embed=embed)
+    @app_commands.command(description= "Get an avatar. Leave empty for your own, or mention someone to get theirs.")
+    async def pfp(self, interaction: discord.Interaction, thatuser: discord.Member = None):
+        target = thatuser or interaction.user
+        embed = make_embed(interaction, f"{target.name}'s avatar", image=target.display_avatar.url, color=discord.Color.blue())
+        await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(mainCommands(bot))
