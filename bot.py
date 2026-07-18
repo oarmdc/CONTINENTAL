04c08275e88e19b47dd6a4146042e93b0f147081
oarmdc <gallaouimohamedomar@gmail.com>
2026-07-19 00:00:04 +0200

    load levels cog in setup_hook for enhanced functionality

--- bot.py
+++ bot.py
@@ -29,6 +29,7 @@ async def setup_hook():
     await bot.load_extension("cogs.events")
     await bot.load_extension("cogs.snipe")
     await bot.load_extension("cogs.adminCommands")
+    await bot.load_extension("cogs.levels")
     await bot.tree.sync()
     node = wavelink.Node(uri=f"http://{os.getenv('LAVALINK_HOST')}:2333", password=os.getenv("LAVALINK_PASSWORD"))
     await wavelink.Pool.connect(nodes=[node], client=bot)
