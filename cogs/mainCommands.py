a655fc559f7a72d155786a2c6e98ef1fe0030a86
oarmdc <gallaouimohamedomar@gmail.com>
2026-07-19 03:41:13 +0200

    feat: add birthday management commands with database integration

--- cogs/mainCommands.py
+++ cogs/mainCommands.py
@@ -1,17 +1,44 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from utils import make_embed
+from utils import make_embed, send_error_embed, send_success_embed
+import calendar
 
 class mainCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+        
+    async def cog_load(self):
+        await self.bot.db.execute("""
+            CREATE TABLE IF NOT EXISTS birthdays (
+                user_id INTEGER,
+                day INTEGER,
+                month INTEGER,
+                PRIMARY KEY (user_id)
+            )
+        """)
+        await self.bot.db.commit()
+
     @app_commands.command(description= "Get an avatar. Leave empty for your own, or mention someone to get theirs.")
     async def pfp(self, interaction: discord.Interaction, thatuser: discord.Member = None):
         target = thatuser or interaction.user
         embed = make_embed(interaction, f"{target.name}'s avatar", image=target.display_avatar.url, color=discord.Color.blue())
         await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(description="Add your birthday - Get a notification every year")
+    async def setbirthday(self, interaction: discord.Interaction, day: app_commands.Range[int, 1, 31], month: app_commands.Range[int, 1, 12]):
+        if day > calendar.monthrange(2024, month)[1]:
+            await send_error_embed(interaction, "That day doesn't exist in that month.")
+            return
+        await self.bot.db.execute("""
+            INSERT INTO birthdays (user_id, day, month)
+            VALUES (?, ?, ?)
+            ON CONFLICT(user_id)
+            DO UPDATE SET day = ?, month = ?
+        """, (interaction.user.id, day, month, day, month))
+        await self.bot.db.commit()
+        await send_success_embed(interaction, f"Birthday saved for {interaction.user.display_name}: **{day:02d}.{month:02d}.** 🎂")
+
 
 async def setup(bot):
     await bot.add_cog(mainCommands(bot))
