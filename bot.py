aeab002af9b2de610f62796b708ac019752f748e
oarmdc <gallaouimohamedomar@gmail.com>
2026-07-23 00:37:10 +0200

    feat: load Valorant cog in setup_hook for enhanced functionality

--- bot.py
+++ bot.py
@@ -32,6 +32,7 @@ async def setup_hook():
     await bot.load_extension("cogs.snipe")
     await bot.load_extension("cogs.adminCommands")
     await bot.load_extension("cogs.levels")
+    await bot.load_extension("cogs.valorant")
     await bot.tree.sync()
     node = wavelink.Node(uri=f"http://{os.getenv('LAVALINK_HOST')}:2333", password=os.getenv("LAVALINK_PASSWORD"))
     await wavelink.Pool.connect(nodes=[node], client=bot)
